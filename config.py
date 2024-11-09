df621ba041ac84ea3bddd423e69b1956c9b9a3ae
Harsha <158590882+Harsh-un@users.noreply.github.com>
2024-11-09 12:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 class Config:
     API_ID=24509589
     API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
-    TOKEN="6572403691:AAFV-3PpCb7N06uqJC2mCdK4QzW6VtV5CeE"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6664582540").split(" "))
+    TOKEN="7979573453:AAEC4McQ5zM9Ae4VcWmqytAqLR1kF57vPAc"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "6018803920").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID=6796545941
-    BOT_USERNAME="ZuliaiBot"
-    BOT_NAME="ZuliaiBot"
+    BOT_ID=7979573453
+    BOT_USERNAME="Gc_actibebot"
+    BOT_NAME="『 ʙᴏɪɪ 』"
